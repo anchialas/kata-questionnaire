740f60e9a2f873c9d8d86ce93492a015bf61252c
Adi König <anchialas@gmail.com>
2022-08-24 12:12:22 +0200

    Read Questions with answers

--- src/main/java/ch/css/kata/questionnaire/QuestionnaireReader.java
+++ src/main/java/ch/css/kata/questionnaire/QuestionnaireReader.java
@@ -4,14 +4,32 @@ import java.io.IOException;
 import java.net.URISyntaxException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+
+import static java.util.stream.Collectors.groupingBy;
 
 public class QuestionnaireReader {
 
     public static final String QUESTIONNAIRE_FILE = "/questionnaire.txt";
+    public static final String QUESTION_MARK = "?";
 
-    public String read() throws URISyntaxException, IOException {
+    List<String> read() throws URISyntaxException, IOException {
         Path filePath = Path.of(getClass().getResource(QUESTIONNAIRE_FILE).toURI());
-        return Files.readString(filePath);
+        return Files.readAllLines(filePath);
 
     }
+
+    public List<List<String>> readPolls() throws URISyntaxException, IOException {
+        int[] index = new int[]{0};
+
+        return new ArrayList<>(read().stream().collect(groupingBy(str -> {
+            if (str.startsWith(QUESTION_MARK)) {
+                index[0]++;
+            }
+            return index[0];
+        })).values());
+    }
+
+
 }
--- src/test/java/ch/css/kata/questionnaire/QuestionnaireReaderTest.java
+++ src/test/java/ch/css/kata/questionnaire/QuestionnaireReaderTest.java
@@ -2,6 +2,8 @@ package ch.css.kata.questionnaire;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 class QuestionnaireReaderTest {
@@ -9,13 +11,21 @@ class QuestionnaireReaderTest {
     private final QuestionnaireReader testee = new QuestionnaireReader();
 
     @Test
-    void read_happy_case() throws Exception {
-        String content = testee.read();
+    void read_all_lines() throws Exception {
+        int expectedLineCount = 8;
 
-        assertThat(content).isEqualTo("hello");
+        List<String> actual = testee.read();
+
+        assertThat(actual).hasSize(expectedLineCount);
     }
 
+    @Test
+    void read_all_polls() throws Exception {
+        int expectedPollCount = 2;
 
+        List<List<String>> actual = testee.readPolls();
 
+        assertThat(actual).hasSize(expectedPollCount);
+    }
 
 }
